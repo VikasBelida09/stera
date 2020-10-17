62e58dfb1bfb34b7030ad1f8dcc9f5608ea612da
Belida Vikas <belida.vikas@accolite.com>
2020-10-17 11:49:58 +0530

    perfectly styled

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="product">
       <Product />
+      </div>
       <MainFeatures />
       <div className="usability">
         <Usability />
