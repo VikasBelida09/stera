a48aaccb428b10bb1b43752319c26cbbdfcac1fd
Belida Vikas <belida.vikas@accolite.com>
2020-12-08 11:04:06 +0530

    addedd build file

---
+++ build/precache-manifest.4b4624871d86d548d57fc0bd23409460.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfeb86e9a24f455b1e699e4e8cbb0569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec58400dd74675f759a",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c1c2b1fc0bee1086e915",
+    "url": "/static/css/main.7054c9f6.chunk.css"
+  },
+  {
+    "revision": "aec58400dd74675f759a",
+    "url": "/static/js/2.b8555b0b.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.b8555b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c2b1fc0bee1086e915",
+    "url": "/static/js/main.fbd23ff9.chunk.js"
+  },
+  {
+    "revision": "472cfdf6f34d4d17b242",
+    "url": "/static/js/runtime-main.b5e243f7.js"
+  },
+  {
+    "revision": "a66586dce9b766a1f78a7322e11e044d",
+    "url": "/static/media/Group21.a66586dc.svg"
+  },
+  {
+    "revision": "f644a7ff7e714abffd846fb57c0db1d3",
+    "url": "/static/media/Path1.f644a7ff.png"
+  },
+  {
+    "revision": "b8d889bf72a11412a922f79d779244d2",
+    "url": "/static/media/Rect.b8d889bf.png"
+  },
+  {
+    "revision": "563fcd6b6796211c84a00c362e07f6b1",
+    "url": "/static/media/amazon.563fcd6b.png"
+  },
+  {
+    "revision": "72c3f9da442def842daecc5f5495a496",
+    "url": "/static/media/baby-cutlery.72c3f9da.svg"
+  },
+  {
+    "revision": "c29023dc3aeaed9e537753c2607181c9",
+    "url": "/static/media/facebook.c29023dc.svg"
+  },
+  {
+    "revision": "03f70fd4588f84fd89afd59fdebbdc32",
+    "url": "/static/media/facebook1.03f70fd4.svg"
+  },
+  {
+    "revision": "9459a14a261bd111bd49ef8733e5a0b0",
+    "url": "/static/media/feeding-bottle.9459a14a.svg"
+  },
+  {
+    "revision": "e32d8aca021ec0244e8f196dd6e3dbd8",
+    "url": "/static/media/hamburger.e32d8aca.svg"
+  },
+  {
+    "revision": "76a6f610db52370469bd9c250db53e8b",
+    "url": "/static/media/image1.76a6f610.png"
+  },
+  {
+    "revision": "1929cff31b505cdac960b0fa79ea2bec",
+    "url": "/static/media/image2.1929cff3.png"
+  },
+  {
+    "revision": "769c1a9b88ba71b1aaf4ba173b3b5c2b",
+    "url": "/static/media/image3.769c1a9b.png"
+  },
+  {
+    "revision": "05705911967c3660d3d8d9edc1492e28",
+    "url": "/static/media/image4.05705911.png"
+  },
+  {
+    "revision": "c58e308ed2e93baeaaabbccad639cb96",
+    "url": "/static/media/instagram.c58e308e.svg"
+  },
+  {
+    "revision": "70709e1a5a65d6ba22eda06dcd0c69dd",
+    "url": "/static/media/instagram1.70709e1a.svg"
+  },
+  {
+    "revision": "ce84a736f7d2e10fd38a4bada6fc2955",
+    "url": "/static/media/linkedin.ce84a736.svg"
+  },
+  {
+    "revision": "f593eabdb310b9e63f0ab62cfdf7157d",
+    "url": "/static/media/linkedin1.f593eabd.svg"
+  },
+  {
+    "revision": "d10731dcfb555a418c19cd7975f87601",
+    "url": "/static/media/logo11.d10731dc.svg"
+  },
+  {
+    "revision": "eb776543305da13d5e78eb4374cb5bae",
+    "url": "/static/media/makeup.eb776543.svg"
+  },
+  {
+    "revision": "64c8a9daace60f3fbd942e9e617bff64",
+    "url": "/static/media/product.64c8a9da.png"
+  },
+  {
+    "revision": "15d0cfc355571bece50efbc4950bd6bc",
+    "url": "/static/media/product1.15d0cfc3.png"
+  },
+  {
+    "revision": "a7f84c10c0a08f8c14f28321a6d0c703",
+    "url": "/static/media/product2.a7f84c10.png"
+  },
+  {
+    "revision": "bcea1d884a7671856dc25843ba7366d7",
+    "url": "/static/media/product3.bcea1d88.png"
+  },
+  {
+    "revision": "8ec904824d96ad683aa04bad69a922ec",
+    "url": "/static/media/product4.8ec90482.png"
+  },
+  {
+    "revision": "c12d2a829a42f7b980ff1d7bd96aaa4a",
+    "url": "/static/media/stera1.c12d2a82.png"
+  },
+  {
+    "revision": "2b4a487adf4c1a466889b35350f9647a",
+    "url": "/static/media/stera2.2b4a487a.png"
+  },
+  {
+    "revision": "d46b22fa522da04f323e42d47514778f",
+    "url": "/static/media/stera3.d46b22fa.png"
+  },
+  {
+    "revision": "51c3bf73addcbc160cf40c474fed44b5",
+    "url": "/static/media/stera4.51c3bf73.png"
+  },
+  {
+    "revision": "d6534f05e70edaf4582e2f8dbd58c97d",
+    "url": "/static/media/sterabgi.d6534f05.png"
+  },
+  {
+    "revision": "caf0b1805e718eaf79e98d4b2733ed63",
+    "url": "/static/media/steracc.caf0b180.png"
+  },
+  {
+    "revision": "3685be205bc93d16f015e60db6b0c59f",
+    "url": "/static/media/steracs.3685be20.png"
+  },
+  {
+    "revision": "2a6a8ea91616ade873a333f4ea24f9b1",
+    "url": "/static/media/steramain.2a6a8ea9.png"
+  },
+  {
+    "revision": "5f69ecdafaddeb72fed791251ae4d0b0",
+    "url": "/static/media/surface1.5f69ecda.png"
+  },
+  {
+    "revision": "fa77875fb38915b02ea9761189f5bb20",
+    "url": "/static/media/surface2.fa77875f.png"
+  },
+  {
+    "revision": "630f93bed4ce60346716a38cb38f0145",
+    "url": "/static/media/svg2.630f93be.svg"
+  },
+  {
+    "revision": "998021d38cee401053d0309b04e8b556",
+    "url": "/static/media/svg3.998021d3.svg"
+  },
+  {
+    "revision": "6ba90e603552c0b631530b36cd5b1e4a",
+    "url": "/static/media/svguv.6ba90e60.svg"
+  },
+  {
+    "revision": "0bf862ea6e68113735f702ffb23cabfc",
+    "url": "/static/media/uvstera.0bf862ea.png"
+  }
+]);
